b277d481388c3d1aa7c95c7dad56e97850a90b8c
Cheeckyio <kwabenaafoa199@gmail.com>
2025-09-18 14:15:42 +0000

    controlflow

---
+++ py_tutorials/controlflow.py
@@ -0,0 +1,35 @@
+#Tutorials from mainly pynative ot w3schools
+"""flow control is the order in which statements or 
+blocks of code are executed at runtime based on a condition.
+control flows are divided into three categories
+1.CONDITIONAL STATEMENTS(if , if else, if elif else, nested if else)--acts depending 
+on a condition being true or false
+
+2.TRANSFER STATEMENTS (break, continue,pass)
+used to alter a programs way of executiion
+
+3.Iterative STATEMENTS (for and while loop)---terative statements allow us to
+execute a block of code repeatedly as long as the condition is True
+"""
+
+#if condtions execute if the condtion given is true
+"""
+number = 6
+if number > 5:
+    print(number * number)
+"""
+    
+    
+"""    
+#if else ---this checks if the (if) condition is false...then it runs
+name = input("Enter your name: ")
+if name == "matthew":
+    print("You are right")
+else:
+    print("Change your name")    
+"""
+
+#CHain multiple if statements
+#    
+    
+    
