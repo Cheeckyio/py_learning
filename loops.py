896076bf26f67e0c6615648b8328c996a6391196
Cheeckyio <kwabenaafoa199@gmail.com>
2025-09-29 05:14:32 +0000

    loops construct

--- loops.py
+++ loops.py
@@ -1,3 +1,5 @@
-x = "matthew"
-for i in x:
-    print(i)
+#loops are used to execute a block  of code a number of times
+#For loops are used to iterate over sequences, which are collection of items
+fruits = ["apple","mango","pear"]
+for fruit in fruits:
+    print(fruit)
